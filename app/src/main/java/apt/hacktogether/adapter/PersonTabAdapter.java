b0f063bf460a80759e6e9a982ff0d0b3239dcd20
DavidTLiau <davidtliau@gmail.com>
2015-12-09 17:09:06 -0600

    messenger from card work

--- app/src/main/java/apt/hacktogether/adapter/PersonTabAdapter.java
+++ app/src/main/java/apt/hacktogether/adapter/PersonTabAdapter.java
@@ -1,6 +1,7 @@
 package apt.hacktogether.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,9 +17,13 @@ import com.parse.ParseRelation;
 import com.parse.ParseUser;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import apt.hacktogether.R;
+import apt.hacktogether.activity.BrowseActivity;
+import apt.hacktogether.activity.MessageActivity;
 import apt.hacktogether.utils.Common;
 import apt.hacktogether.utils.ParseUtils;
 import butterknife.Bind;
@@ -44,12 +49,6 @@ public class PersonTabAdapter extends BaseAdapter {
         public ViewHolder(View view) {
             ButterKnife.bind(this, view);
             chatRoomButton = (Button) view.findViewById(R.id.btn_chat_room);
-            chatRoomButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // TODO: go to MessageActivity
-                }
-            });
         }
     }
 
@@ -96,6 +95,16 @@ public class PersonTabAdapter extends BaseAdapter {
         holder.txtPersonName.setText(hacker.getString(Common.OBJECT_USER_NICK));
         getHackerInterests(hacker, holder);
         getHackerSkills(hacker, holder);
+        holder.chatRoomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MessageActivity.class);
+                ArrayList<String> list = new ArrayList<String>(Arrays.asList(hacker.getObjectId()));
+                list.add(ParseUser.getCurrentUser().getObjectId());
+                intent.putStringArrayListExtra("mTargetParticipants", list);
+                v.getContext().startActivity(intent);
+            }
+        });
 
         if (ParseUser.getCurrentUser().getObjectId().equals(hacker.getObjectId())) holder.chatRoomButton.setVisibility(View.GONE);
         return convertView;
